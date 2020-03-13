f465403400747b71e64b232449f6d2982c167370
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-13 22:58:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,31 +1,24 @@
-class Doctor
-
+class Doctor 
   attr_accessor :name 
-
   @@all = []
-
   def initialize(name)
     @name = name 
     @@all << self 
   end 
 
-  def self.all 
+   def self.all 
     @@all 
   end 
 
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
+   def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
   end 
 
-  def appointments 
-    Appointment.all.select do |appointment|
-      appointment.doctor == self 
-    end
+   def appointments 
+    Appointment.all.select {|a| a.doctor == self} 
   end 
 
-  def patients 
-    appointments.map do |appointment|
-      appointment.patient
-    end 
+   def patients 
+    appointments.map {|a| a.patient}
   end 
-end  
+end
