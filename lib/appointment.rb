f3b8cdb3d1693b153844dd3044a0dcf97a3df685
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-13 22:58:47 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,14 @@
-class Appointment
-
-  attr_accessor :date, :patient, :doctor
-
+class Appointment 
+  attr_accessor :date, :patient, :doctor 
   @@all = []
-
   def initialize(date, patient, doctor)
     @date = date
-    @patient = patient
-    @doctor = doctor
-    @@all << self
-  end
+    @patient = patient 
+    @doctor = doctor 
+    @@all << self 
+  end 
 
-  def self.all
-    @@all
-  end
+   def self.all 
+    @@all 
+  end 
 end 
